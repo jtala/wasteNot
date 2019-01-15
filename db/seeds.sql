562767faf2b6171199f89ad5c7875f2282e89683
KA <kareemalasmar@gmail.com>
2019-01-15 11:53:59 -0800

    updated schema and created seeds for customer table

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO customer_requests (customer_name, item_name, estimated_weight, item_location)
+VALUES ("Jonathan", "Water Bottles", 30, "Westwood");
+
+INSERT INTO customer_requests (customer_name, item_name, estimated_weight, item_location)
+VALUES ("Clint", "Aluminum", 45, "Santa Monica");
+
+INSERT INTO customer_requests (customer_name, item_name, estimated_weight, item_location)
+VALUES ("ALejandro", "Plastic", 15, "Longbeach");
+
+INSERT INTO customer_requests (customer_name, item_name, estimated_weight, item_location)
+VALUES ("Kareem", "Aluminum", 20, "Hollywood");
+
+INSERT INTO customer_requests (customer_name, item_name, estimated_weight, item_location)
+VALUES ("Clark", "Copper", 10, "Westwood");
+
+INSERT INTO customer_requests (customer_name, item_name, estimated_weight, item_location)
+VALUES ("Peter", "Water Bottles", 35, "Longbeach");
