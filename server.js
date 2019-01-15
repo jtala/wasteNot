b71f6e7bcd1957410f20d2d773d8e18c2f10aae2
Jonathan Talavera <jtalavera@g.ucla.edu>
2019-01-15 10:24:04 -0800

    server js is setup waiting on config

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependcies
+var express = require("express");
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+// More Dependency Installation..
+
+// img reference is static
+app.use(express.static("public"));
+// JSON translator
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// install handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// router. To finish according to file name
+//var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
