19a090961bf7e956db6ea3f7e85ed581d7120243
KA <kareemalasmar@gmail.com>
2019-01-15 10:45:49 -0800

    set up db schema

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS wasteNot_db;
+CREATE DATABASE wasteNot_db;
+USE wasteNot_db;
+
+CREATE TABLE customer_requests (
+    request_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(30) NOT NULL,
+    item_name VARCHAR(30) NOT NULL,
+    estimated_weight INT NOT NULL,
+    item_location VARCHAR(100) NOT NULL,
+    request_status BOOLEAN DEFAULT FALSE,
+    drivers_id INT
+);
+
+CREATE TABLE drivers (
+    driver_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    driver_name VARCHAR(30) NOT NULL
+);
