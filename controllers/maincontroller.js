3ced05219caeb7941f8d9969ab0860d1ecc06a60
KA <kareemalasmar@gmail.com>
2019-01-16 08:01:45 -0800

    added views html

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -3,17 +3,34 @@ var express = require("express");
 var router = express.Router();
 var customer = require("../models/customer.js");
 
-//Home Page, gets All the burgers.
+// redirects to home
 router.get("/", function(req, res) {
+  res.redirect("/home");
+});
 
-customer.selectall(function(data) {
+//This Page gets all the customer request data.
+router.get("/home", function(req, res) {
+    res.render("index");
+});
+
+router.get("/drive", function(req, res) {
+  customer.selectall(function(data) {
     var hbsObject = {
       customers: data
     };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+    res.render("drive", hbsObject);
   });
 });
+
+router.get("/requests", function(req, res) {
+  customer.selectall(function(data) {
+    var hbsObject = {
+      customers: data
+    };
+    res.render("requests", hbsObject);
+  });
+});
+
 
 
 // Exports to server.js
