1db60d1936d55feec9a06c0ce43b55318108dbf9
Jonathan Talavera <jtalavera@g.ucla.edu>
2019-01-18 14:22:57 -0800

    trying to add associations

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -7,12 +7,25 @@ var db = require("../models");
 
 router.get("/", function (req, res) {
   res.render("index");
-  
-
-
-
-  
 });
+
+
+//  ----------------------- Logins
+
+// Injects login information onto Customers table & reroutes to requests.
+router.post("/api/login", function (req, res) {
+  db.Customer.create(req.body);
+});
+
+// Going to this url shows JSON of all the login information
+router.get("/api/login", function (req, res) {
+  db.Customer.findAll({})
+    .then(function (dbLogins) {
+      res.json(dbLogins);
+    });
+});
+
+
 
 //  ----------------------- Drivers 
 
@@ -46,7 +59,7 @@ router.get("/requests", function (req, res) {
 });
 
 
-// Going to api shows entire object.
+// URL shows JSON of all requests.
 router.get("/api/requests", function (req, res) {
   db.Request.findAll({})
     .then(function (dbRequest) {
@@ -58,19 +71,6 @@ router.get("/api/requests", function (req, res) {
 router.post("/api/requests", function (req, res) {
   db.Request.create(req.body);
 });
-
-
-
-
-//  ----------------------- Tracking
-
-
-// adding route to take to tracking page
-router.get("/track", function (req, res) {
-  res.render("track");
-});
-
-
 
 // Exports to server.js
 module.exports = router;
